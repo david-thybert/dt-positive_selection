bcff106cd511142199eb28d677e8304449f771e4
David Thybert <dthybert@Davids-MacBook-Air.local>
2024-11-21 14:38:05 +0000

    add filtration of possible annotaiton error

--- tools/select_significant_hits.py
+++ tools/select_significant_hits.py
@@ -2,14 +2,49 @@ import sys
 import argparse
 import pandas as pd
 
-def main(possel:str, threshold:float, out:str)-> None:
+
+
+def is_sequential_pos(row:object,number:int,windows:int)-> bool:
+    """
+    """
+    pos_string = row["position"]
+    positions = pos_string.split("|")
+    last_pos = -1
+    nb = 0
+    i=0
+    while i < len(positions):
+        p = positions[i]
+        pos = int(p.split(":")[0])
+        lst_pos = [pos]
+        j = i + 1
+        while j < len(positions):
+            p_next = positions[j]
+            pos_next = int(p_next.split(":")[0])
+            if pos_next - pos <=windows:
+                lst_pos.append(pos_next)
+            else:
+                if len(lst_pos)>=number:
+                    return True
+                break
+            j = j +1
+        i = i +1
+    return False
+
+
+def main(possel:str, threshold:float, nb:int, window:int, out:str)-> None:
     """
     """
     df = pd.read_csv(possel, index_col=0, sep="\t")
     print(df.head())
     df['pval_adj'] = df['pval_adj'].astype(float)
-    filteres_Df = df[df["pval_adj"] <= threshold]
-    filteres_Df.to_csv(out, sep="\t", index=False)
+    significant_df = df[df["pval_adj"] <= threshold]
+
+    filtered_df = pd.DataFrame()
+    for index, row in significant_df.iterrows():
+        if not is_sequential_pos(row, nb, window):
+            filtered_df.append(row, ignore_index=True)
+    filtered_df.to_csv(out, sep="\t", index=False)
+
 
 
 
@@ -21,6 +56,8 @@ def main(possel:str, threshold:float, out:str)-> None:
 parser = argparse.ArgumentParser(description='Script formating the data to be handle by the positvie selction pipeline')
 parser.add_argument('--possel',type=str, help='possel file')
 parser.add_argument('--thr',type=float, default=0.05, help='adj pval threshold to filter')
+parser.add_argument('--nb',type=int, help='possel file')
+parser.add_argument('--wind',type=int, default=0.05, help='adj pval threshold to filter')
 parser.add_argument('--out', type=str, help='outfile')
 
 args = parser.parse_args()
